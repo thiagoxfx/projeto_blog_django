46aa646aba8c94e6b940d8e74c0af9f2bee726ef
Thiago Guimaraes <thiagoxfx@gmail.com>
2026-02-08 19:31:26 -0300

    search

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,6 +1,7 @@
 from blog.models import Post
 from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.db.models import Q
 
 PER_PAGE = 9
 
@@ -65,6 +66,27 @@ def tag(request, slug):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+        }
+    )
+
+def search(request):
+    search_value = request.GET.get('search').strip()
+    posts = (
+        Post.objects.get_published()\
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[0:PER_PAGE]
+    )
+    
+ 
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
         }
     )
 
