f3d8b00eab5ff7bf906a5ee581cd8c9cd6b10e03
Thiago Guimaraes <thiagoxfx@gmail.com>
2026-02-12 21:50:23 -0300

    page titles

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,14 +1,16 @@
-from blog.models import Post
+from blog.models import Page, Post
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator
-from django.shortcuts import render
 from django.db.models import Q
+from django.http import Http404
+from django.shortcuts import render
 
 PER_PAGE = 9
 
 
 def index(request):
     posts = Post.objects.get_published()
-    
+
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -18,13 +20,25 @@ def index(request):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': 'Home - ',
         }
     )
+
 
 def created_by(request, author_pk):
+    user = User.objects.filter(pk=author_pk).first()
+
+    if user is None:
+        raise Http404()
+
     posts = Post.objects.get_published()\
-    .filter(created_by__pk=author_pk)
-    
+        .filter(created_by__pk=author_pk)
+    user_full_name = user.username
+
+    if user.first_name:
+        user_full_name = f'{user.first_name} {user.last_name}'
+    page_title = 'Posts de ' + user_full_name + ' - '
+
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -34,86 +48,122 @@ def created_by(request, author_pk):
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
         }
     )
+
 
 def category(request, slug):
     posts = Post.objects.get_published()\
         .filter(category__slug=slug)
-    
+
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+
+    if len(page_obj) == 0:
+        raise Http404()
+
+    page_title = f'{page_obj[0].category.name} - Categoria - '
 
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
         }
     )
+
 
 def tag(request, slug):
     posts = Post.objects.get_published()\
         .filter(tags__slug=slug)
-    
+
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+
+    if len(page_obj) == 0:
+        raise Http404()
+
+    page_title = f'{page_obj[0].tags.first().name} - Tag - '
 
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': page_obj,
+            'page_title': page_title,
         }
     )
 
+
 def search(request):
-    search_value = request.GET.get('search').strip()
+    search_value = request.GET.get('search', '').strip()
+
     posts = (
-        Post.objects.get_published()\
+        Post.objects.get_published()
         .filter(
             Q(title__icontains=search_value) |
             Q(excerpt__icontains=search_value) |
             Q(content__icontains=search_value)
-        )[0:PER_PAGE]
+        )[:PER_PAGE]
     )
-    
- 
+
+    page_title = f'{search_value[:30]} - Search - '
+
     return render(
         request,
         'blog/pages/index.html',
         {
             'page_obj': posts,
             'search_value': search_value,
+            'page_title': page_title,
         }
     )
 
 
 def page(request, slug):
+    page_obj = (
+        Page.objects
+        .filter(is_published=True)
+        .filter(slug=slug)
+        .first()
+    )
+
+    if page_obj is None:
+        raise Http404()
+
+    page_title = f'{page_obj.title} - Página - '
+
     return render(
         request,
         'blog/pages/page.html',
         {
-            # 'page_obj': page_obj,
+            'page': page_obj,
+            'page_title': page_title,
         }
     )
 
 
 def post(request, slug):
-    post = (
+    post_obj = (
         Post.objects.get_published()
         .filter(slug=slug)
         .first()
     )
 
+    if post_obj is None:
+        raise Http404()
 
+    page_title = f'{post_obj.title} - Post - '
 
     return render(
         request,
         'blog/pages/post.html',
         {
-            'post': post,
+            'post': post_obj,
+            'page_title': page_title,
         }
     )
